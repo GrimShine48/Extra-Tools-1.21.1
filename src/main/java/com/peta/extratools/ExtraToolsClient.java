ad8f3eccd0815e45452a3dec1be9ec80eae2cdd6
GrimShine48 <s.pornpawit@gmail.com>
2024-10-26 18:41:03 +0700

    Initial commit

---
+++ src/main/java/com/peta/extratools/ExtraToolsClient.java
@@ -0,0 +1,10 @@
+package com.peta.extratools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtraToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
