ad0b6cc5d81efebbc55fcf6f3d448a588ec4636e
GrimShine48 <s.pornpawit@gmail.com>
2024-11-24 09:08:32 +0700

    Initial changes

--- src/main/java/com/peta/extratools/item/custom/ScytheItem.java
+++ src/main/java/com/peta/extratools/item/custom/ScytheItem.java
@@ -1,90 +1,20 @@
 package com.peta.extratools.item.custom;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-import com.mojang.datafixers.util.Pair;
-
-import java.util.*;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
-import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.world.World;
-import net.minecraft.world.event.GameEvent;
-import net.minecraft.world.event.GameEvent.Emitter;
 import com.peta.extratools.util.ModTags;
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 
-import java.util.Map;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScytheItem extends MiningToolItem {
-    protected static final HashMap TILLING_ACTIONS;
-
     public ScytheItem(ToolMaterial material, Settings settings) {
         super(material, ModTags.Blocks.SCYTHE_MINEABLE, settings);
-    }
-
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        World world = context.getWorld();
-        BlockPos blockPos = context.getBlockPos();
-        Pair<Predicate<ItemUsageContext>, Consumer<ItemUsageContext>> pair = (Pair)TILLING_ACTIONS.get(world.getBlockState(blockPos).getBlock());
-        if (pair == null) {
-            return ActionResult.PASS;
-        } else {
-            Predicate<ItemUsageContext> predicate = (Predicate)pair.getFirst();
-            Consumer<ItemUsageContext> consumer = (Consumer)pair.getSecond();
-            if (predicate.test(context)) {
-                PlayerEntity playerEntity = context.getPlayer();
-                world.playSound(playerEntity, blockPos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
-                if (!world.isClient) {
-                    consumer.accept(context);
-                    if (playerEntity != null) {
-                        context.getStack().damage(1, playerEntity, LivingEntity.getSlotForHand(context.getHand()));
-                    }
-                }
-
-                return ActionResult.success(world.isClient);
-            } else {
-                return ActionResult.PASS;
-            }
-        }
-    }
-
-    public static Consumer<ItemUsageContext> createTillAction(BlockState result) {
-        return (context) -> {
-            context.getWorld().setBlockState(context.getBlockPos(), result, 11);
-            context.getWorld().emitGameEvent(GameEvent.BLOCK_CHANGE, context.getBlockPos(), Emitter.of(context.getPlayer(), result));
-        };
-    }
-
-    public static Consumer<ItemUsageContext> createTillAndDropAction(BlockState result, ItemConvertible droppedItem) {
-        return (context) -> {
-            context.getWorld().setBlockState(context.getBlockPos(), result, 11);
-            context.getWorld().emitGameEvent(GameEvent.BLOCK_CHANGE, context.getBlockPos(), Emitter.of(context.getPlayer(), result));
-            Block.dropStack(context.getWorld(), context.getBlockPos(), context.getSide(), new ItemStack(droppedItem));
-        };
-    }
-
-    public static boolean canTillFarmland(ItemUsageContext context) {
-        return context.getSide() != Direction.DOWN && context.getWorld().getBlockState(context.getBlockPos().up()).isAir();
-    }
-
-    static {
-        TILLING_ACTIONS = Maps.newHashMap(ImmutableMap.of(Blocks.GRASS_BLOCK, Pair.of(HoeItem::canTillFarmland, createTillAction(Blocks.FARMLAND.getDefaultState())), Blocks.DIRT_PATH, Pair.of(HoeItem::canTillFarmland, createTillAction(Blocks.FARMLAND.getDefaultState())), Blocks.DIRT, Pair.of(HoeItem::canTillFarmland, createTillAction(Blocks.FARMLAND.getDefaultState())), Blocks.COARSE_DIRT, Pair.of(HoeItem::canTillFarmland, createTillAction(Blocks.DIRT.getDefaultState())), Blocks.ROOTED_DIRT, Pair.of((Predicate)(itemUsageContext) -> true, createTillAndDropAction(Blocks.DIRT.getDefaultState(), Items.HANGING_ROOTS))));
     }
 
     public static List<BlockPos> getBlocksToBeDestroyed(int range, BlockPos initalBlockPos, ServerPlayerEntity player) {
